21b67794965df30f24f13ccb83e9f2d8dd4230ff
Sdatta-2006 <snehomita.datta2024@uem.edu.in>
2026-03-06 15:41:10 +0530

    Add files via upload
    it calculates the minimum element in an array

---
+++ Arrays/Minimum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Minimum {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int l=sc.nextInt();
+        int a[]=new int[l];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0;i<l;i++){
+            a[i]=sc.nextInt();
+        }
+        sc.close();
+        int min=a[0];
+        for(int i=1;i<l;i++){
+            if(min>a[i]){
+                min=a[i];
+            }
+        }
+        System.out.printf("The minimum element in the array: %d",min);
+    }
+}
